7e28a2dda342d2dc934f1183d68cb5ccb16e1051
Herson Salinas <hersonhn@gmail.com>
2019-01-15 20:09:14 -0600

    If the url is alredy on the db, return it

--- modules/create-short-url.js
+++ modules/create-short-url.js
@@ -1,18 +1,51 @@
 
-const getTitle = require('get-title-at-url');
+const getTitleAtURL = require('get-title-at-url');
 const PouchDB = require('pouchdb-node');
+PouchDB.plugin(require('pouchdb-find'));
 
 const generator = require('../scripts/url-generator');
 const db = new PouchDB(process.env.SHORTENER_CONNECTION_STRING);
 
+
 module.exports = function (req, res) {
-  let { url, title } = req.body;
+  let { url } = req.body;
+
   let short = generator.generateURL();
 
-  getTitle(url, (title) => {
-    db.put({ _id: short, short, title, long: url, count: 0 })
-      .then((data) => res.send({ url: short }))
-      .catch((e) => res.send({ error: e }));
-  });
+  findIfExists(url)
+    .then(function(doc) {
+      if (doc) return doc;
+      return createNew(url, short);
+    })
+    .then((doc) => res.send({ url: doc.short }))
+    .catch((e) => res.send({ error: e }));
+}
 
+
+function findIfExists(url) {
+  return db.find({
+    selector: {
+      long: url,
+    }
+  }).then(results => {
+    let [doc] = results.docs;
+    return doc;
+  });
+}
+
+
+function createNew(url, short) {
+  return getTitle(url)
+    .then((title) => db.put({ _id: short, short, title, long: url, count: 0 }))
+    .then(() => ({ short }));
+}
+
+
+function getTitle(url) {
+  return new Promise(function (resolve, reject) {
+    getTitleAtURL(url, (title, error) => {
+      if (error) return reject(error);
+      resolve(title);
+    });
+  });
 }
--- modules/graphql.js
+++ modules/graphql.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+const PouchDB = require('pouchdb-node');
 
 const express_graphql = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const content = fs.readFileSync(path.join(__dirname, './schema.gql'), 'utf8');
 const schema = buildSchema(content);
+
+const db = new PouchDB(process.env.SHORTENER_CONNECTION_STRING);
 
 // Root resolver
 var root = {
