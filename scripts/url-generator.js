0791917f9344abf4308f3d346717666b260f7cf4
Herson Salinas <hersonhn@gmail.com>
2019-01-15 22:54:23 -0600

    8 digits for random numbers

--- scripts/url-generator.js
+++ scripts/url-generator.js
@@ -16,10 +16,10 @@ function generateNumber() {
   let clutter = 1547532000000 // 2018-01-15 00:00:00;
   let timestamp = (new Date()) - clutter;
 
-  // we also concatenate a random five digits number to the timestamp so
+  // we also concatenate a random eight digits number to the timestamp so
   // there's less chance to get duplicated numbers when two requests come
   // at the same time
-  let random = fiveDigitsNumber();
+  let random = eightDigitsNumber();
 
   // the random number is added first so the generated ids in sequence don't
   // look similar
@@ -29,9 +29,9 @@ function generateNumber() {
 }
 
 
-function fiveDigitsNumber() {
-  let m = 10000;
-  let m2 = m * 10 - 1 - m; // 89999
+function eightDigitsNumber() {
+  let m = 10000000;
+  let m2 = m * 10 - 1 - m; // 89,999,999
   let random = Math.ceil(Math.random() * m2) + m;
   return random;
 }
